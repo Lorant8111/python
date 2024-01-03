d3e35f80fdb677af639b7ea4e386e92a24dadb42
PSELivius <153958008+PSELivius@users.noreply.github.com>
2024-01-03 10:49:32 +0100

    Update
    -

--- Magánhangzó számláló/mghszamlalo.py
+++
@@ -1,8 +0,0 @@
-mondat=input("Adj meg egy mondatot: ")
-mgh=0
-for betu in mondat:
-    if betu in "aeiouAEIOUáéíóöőúüűÁÉÍÓÖŐÚÜŰ":
-        mgh+=1
-
-print("Mondatod: ", mondat)
-print("Magánhangzók száma: ", mgh)
